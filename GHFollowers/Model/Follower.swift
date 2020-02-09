c2e0fddc2299d9bf4b933af72850c66b48e2e64b
Jason Mitchell <devjmitchell@gmail.com>
2020-02-09 13:25:40 -0700

    Create models for network calls prep

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Jason Mitchell on 2/9/20.
+//  Copyright © 2020 Jason Mitchell. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
