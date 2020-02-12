8eb7a7d86ad0b4dcb22b387b9f4e5edee74b8485
Jason Mitchell <devjmitchell@gmail.com>
2020-02-11 22:14:41 -0700

    Create GFUserInfoHeaderVC and use in UserInfoVC

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Jason Mitchell on 2/11/20.
+//  Copyright © 2020 Jason Mitchell. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
