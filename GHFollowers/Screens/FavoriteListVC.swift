54548123e65e2f18ec4fe601d4e5fc804d4b8491
Jason Mitchell <devjmitchell@gmail.com>
2020-02-14 08:17:05 -0700

    Make PersistenceManager work

--- GHFollowers/Screens/FavoriteListVC.swift
+++ GHFollowers/Screens/FavoriteListVC.swift
@@ -13,5 +13,14 @@ class FavoriteListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
+        
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case .failure(let error):
+                break
+            }
+        }
     }
 }
